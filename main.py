a10d795e6ed479baa37a7c50f1aa6a3d3c901b06
Joonas Reinikka <joonas.reinikka@gmail.com>
2018-11-12 00:43:29 +0200

    Initial start screen

---
+++ main.py
@@ -0,0 +1,30 @@
+import kivy
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from screens.start import StartScreen
+from kivy.config import Config
+
+Config.set('graphics', 'width', '800')
+Config.set('graphics', 'height', '480')
+kivy.require('1.0.7')
+
+
+class Photobooth(App):
+    state = {
+        "frame": 0
+    }
+
+    frames = [
+        "./assets/frame1.png",
+        "./assets/frame2.png"
+    ]
+
+    def build(self):
+        sm = ScreenManager()
+        sm.add_widget(StartScreen(name='start'))
+        return sm
+
+
+if __name__ == '__main__':
+    Photobooth().run()
---
+++ screens/start.py
@@ -0,0 +1,44 @@
+from kivy.uix.screenmanager import Screen
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.loader import Loader
+import os
+
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+Builder.load_file(os.path.join(__location__, 'start.kv'))
+
+
+class StartScreen(Screen):
+    def on_enter(self):
+        self.a = App.get_running_app()
+        self.update_frame()
+
+    def next_frame(self):
+        frame_number = self.a.state['frame']
+        next_frame = frame_number + 1
+
+        if next_frame > len(self.a.frames) - 1:
+            self.a.state['frame'] = 0
+        else:
+            self.a.state['frame'] = next_frame
+
+        self.update_frame()
+
+    def prev_frame(self):
+        frame_number = self.a.state['frame']
+        next_frame = frame_number - 1
+
+        if next_frame < 0:
+            self.a.state['frame'] = len(self.a.frames) - 1
+        else:
+            self.a.state['frame'] = next_frame
+
+        self.update_frame()
+
+    def get_frame_source(self):
+        return self.a.frames[self.a.state['frame']]
+
+    def update_frame(self):
+        self.ids.frame.source = self.get_frame_source()
