22b027c978fc384a90547ecf4e98f586206ec3c2
Joonas Reinikka <joonas.reinikka@gmail.com>
2018-11-20 20:50:40 +0200

    Add initial photo screen

--- screens/photo.py
+++ screens/photo.py
@@ -3,6 +3,9 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.loader import Loader
 from kivy.clock import Clock
+from kivy.properties import NumericProperty
+from kivy.animation import Animation
+
 
 import os
 import time
@@ -18,11 +21,23 @@ class PhotoScreen(Screen):
     buttonLabel = "Start countdown"
     timerStart = 0
     timerStarted = False
+    flash_opacity = NumericProperty(0)
+    countdown_started = False
+    flash_animation = Animation(x=100, y=100)
 
     def countdown_func(self, dt):
-        print(self.countdown)
         self.countdown -= 1
+        if self.countdown <= 0:
+            Clock.unschedule(self.countdown_interval)
+            self.take_photo()
+
         self.ids.countdown_button.text = str(self.countdown)
 
     def start_countdown(self):
-        Clock.schedule_interval(self.countdown_func, 1.0)
+        if self.countdown_started is not True: 
+            self.countdown_started = True
+            self.countdown_interval = Clock.schedule_interval(self.countdown_func, 1.0)
+
+    def take_photo(self):
+        print("bOOM")
+        self.flash_opacity = 0.5
