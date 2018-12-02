b58b8be9270033c0bfed144689e7bfdfdc1c07d7
Joonas Reinikka <joonas.reinikka@gmail.com>
2018-12-02 13:49:43 +0200

    Add flash animation and take photo

--- screens/photo.py
+++ screens/photo.py
@@ -23,7 +23,10 @@ class PhotoScreen(Screen):
     timerStarted = False
     flash_opacity = NumericProperty(0)
     countdown_started = False
-    flash_animation = Animation(x=100, y=100)
+    flash_animation = Animation(flash_opacity=0)
+
+    def on_enter(self):
+        self.flash_animation.bind(on_complete=self.next_screen)
 
     def countdown_func(self, dt):
         self.countdown -= 1
@@ -34,10 +37,21 @@ class PhotoScreen(Screen):
         self.ids.countdown_button.text = str(self.countdown)
 
     def start_countdown(self):
-        if self.countdown_started is not True: 
+        if self.countdown_started is not True:
             self.countdown_started = True
-            self.countdown_interval = Clock.schedule_interval(self.countdown_func, 1.0)
+            self.countdown_interval = Clock.schedule_interval(
+                self.countdown_func, 1.0)
 
     def take_photo(self):
-        print("bOOM")
-        self.flash_opacity = 0.5
+        camera = self.ids['camera']
+
+        camera.play = False
+        timestr = time.strftime("%Y%m%d_%H%M%S")
+        # Change this to Raspberry Pi's own camera capture when used in that
+        camera.export_to_png("./photos/IMG_{}.png".format(timestr))
+
+        self.flash_opacity = 1
+        self.flash_animation.start(self)
+
+    def next_screen(self, *args):
+        self.manager.current = 'preview'
