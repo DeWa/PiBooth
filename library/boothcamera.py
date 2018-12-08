35f106bdb6efa48d774bf0603258d16c1721c1fb
Joonas Reinikka <joonas.reinikka@gmail.com>
2018-12-09 01:21:29 +0200

    Lower camera video resolution

--- library/boothcamera.py
+++ library/boothcamera.py
@@ -1,13 +1,8 @@
-#from lib.vendor.camerabase import Camera
-from lib.vendor.camerauix import Camera
-
-from pprint import pprint
-pprint(vars(Camera))
-
+from library.vendor.camerauix import Camera
 
 class BoothCamera(Camera):
     def __init__(self):
-        super().__init__(id="camera", resolution=(1280, 720), size=(800, 600), play=True)
+        super().__init__(id="camera", resolution=(640, 480), size=(800, 600), play=True)
 
     def take_picture(self, path):
         self._camera.take_picture(path)
