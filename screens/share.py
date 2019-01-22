dc31faac513ca3f49e45484191031b5d92823e55
Joonas Reinikka <joonas.reinikka@gmail.com>
2019-01-22 22:32:28 +0200

    Restylize Share screen and add countdown to reset

--- screens/share.py
+++ screens/share.py
@@ -1,6 +1,11 @@
 from kivy.uix.screenmanager import Screen
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.clock import Clock
+from kivy.properties import StringProperty
+from components.qrcode import QRCodeWidget
+
+
 
 import os
 import time
@@ -12,5 +17,16 @@ Builder.load_file(os.path.join(__location__, 'share.kv'))
 
 
 class ShareScreen(Screen):
+    imageurl = StringProperty("")
+
+    def __init__(self, **kwargs):
+        super(ShareScreen, self).__init__(**kwargs)
+
     def on_enter(self):
         self.app = App.get_running_app()
+        print(self.app.config)
+        self.reset_timer = Clock.schedule_interval(
+                self.reset_app, 20)
+    
+    def reset_app(self, dt):
+        self.manager.current = 'start'
