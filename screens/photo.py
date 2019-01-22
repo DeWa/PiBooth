c59648cd45609015c91e3a8b2056a242d21029e8
Joonas Reinikka <joonas.reinikka@gmail.com>
2019-01-22 20:36:41 +0200

    Restylize photo screen

--- screens/photo.py
+++ screens/photo.py
@@ -6,12 +6,13 @@ from kivy.clock import Clock
 from kivy.properties import NumericProperty
 from kivy.animation import Animation
 from kivy.uix.camera import Camera
-from library.progress import ProgressSpinner
-from library.imageutil import PhotoThread, convert_photo
+from components.progress import ProgressSpinner
+from utils.imageutil import PreviewThread, make_preview
 
 import os
 import time
 import threading
+
 
 __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
@@ -20,27 +21,31 @@ Builder.load_file(os.path.join(__location__, 'photo.kv'))
 
 
 class PhotoScreen(Screen):
-    buttonLabel = "Start countdown"
     flash_opacity = NumericProperty(0)
     flash_animation = Animation(flash_opacity=0)
+    countdown = NumericProperty(0)
 
     def on_enter(self):
         self.countdown = 5
-        self.buttonLabel = "Start countdown"
-        self.ids.countdown_button.text = "Start countdown"
         self.timerStart = 0
         self.timerStarted = False
         self.countdown_started = False
 
         self.app = App.get_running_app()
         self.camera = self.app.camera
+        self.camera.size_hint = (1, 1)
         self.camera.play = True
 
         # Add widgets
-        self.ids["camera_wrapper"].add_widget(self.camera, 3)
+        self.ids["camera_wrapper"].add_widget(self.camera, 4)
         self.ids["camera_wrapper"].remove_widget(self.ids["loading_spinner"])
         self.ids["frame"].opacity = 1
         self.ids["countdown_button"].opacity = 1
+        self.ids["back_button"].opacity = 1
+        self.ids["countdown_label"].opacity = 0
+
+        self.ids["countdown_button"].disabled = False
+        self.ids["back_button"].disabled = False
 
         self.update_frame()
 
@@ -50,20 +55,27 @@ class PhotoScreen(Screen):
             Clock.unschedule(self.countdown_interval)
             self.take_photo()
 
-        self.ids.countdown_button.text = str(self.countdown)
-
     def start_countdown(self):
         if self.countdown_started is not True:
             self.countdown_started = True
             self.countdown_interval = Clock.schedule_interval(
                 self.countdown_func, 1.0)
 
+        self.ids["countdown_label"].opacity = 1
+        # Hide buttons
+        self.ids["countdown_button"].opacity = 0
+        self.ids["back_button"].opacity = 0
+        self.ids["countdown_button"].disabled = True
+        self.ids["back_button"].disabled = True
+
     def take_photo(self):
         # Remove photo related widgets
         self.camera.play = False
         self.ids["camera_wrapper"].remove_widget(self.camera)
         self.ids["frame"].opacity = 0
+        self.ids["countdown_label"].opacity = 0
         self.ids["countdown_button"].opacity = 0
+        self.ids["back_button"].opacity = 0
 
         # Save Photo
         timestr = time.strftime("%Y%m%d_%H%M%S")
@@ -73,23 +85,19 @@ class PhotoScreen(Screen):
 
         # Add loading spinner
         self.flash_opacity = 1
-        self.ids["camera_wrapper"].add_widget(self.ids["loading_spinner"], 3)
+        self.ids["camera_wrapper"].add_widget(self.ids["loading_spinner"], 4)
         self.flash_animation.start(self)
 
-        # Convert photo
-        convert_thread = PhotoThread(
-            name='convert_thread',
-            target=convert_photo,
+        # Make preview
+        preview_thread = PreviewThread(
+            name='preview_thread',
+            target=make_preview,
             callback=self.next_screen,
-            callback_args=("hello", "world"),
             args=(timestr, self.get_frame_source(),)
         )
-        convert_thread.start()
-    
-    def wait_animation(self):
-        self.flash_animation.bind(on_complete=self.next_screen)
+        preview_thread.start()
 
-    def next_screen(self, *args, **kwargs):
+    def next_screen(self):
         self.manager.current = 'preview'
 
     def get_frame_source(self):
